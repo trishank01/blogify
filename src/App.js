0e29f1549fab2f927ab0a349d301845a06e28fc7
tk <trishank01@gmail.com>
2023-09-16 17:23:14 +0530

    added menu and blog design

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main >
+    <header>
+      <a href="/" className="logo">MyBlog</a>
+      <nav>
+        <a href="/">Login</a>
+        <a href="/">Register</a>
+      </nav>
+    </header>
+      <div className="post">
+       <div className="image">
+       <img src="https://techcrunch.com/wp-content/uploads/2023/03/GettyImages-1180708314-e1679675797562.jpg?w=1390&crop=1" alt="image1"/>
+       </div>
+       <div className="text">
+       <h2>Web3 adoption could come via the enterprise, but the real boom will be through</h2>
+       <p className="info">
+        <a href="/" className="author" >  john cena </a>
+        <time>2023-01-6 14</time>
+       </p>
+         <p className="summary">As the race for enterprise adoption in web3 accelerates, some people believe it’s not one-sided growth, but a journey where both mainstream enterprises and crypto startups can bring on new opportunities.</p>
+       </div>
+      </div>
+
+      <div className="post">
+       <div className="image">
+       <img src="https://techcrunch.com/wp-content/uploads/2023/03/GettyImages-1180708314-e1679675797562.jpg?w=1390&crop=1" alt="image1"/>
+       </div>
+       <div className="text">
+       <h2>Web3 adoption could come via the enterprise, but the real boom will be through</h2>
+       <p className="info">
+        <a href="/" className="author" >  john cena </a>
+        <time>2023-01-6 14</time>
+       </p>
+         <p className="summary">As the race for enterprise adoption in web3 accelerates, some people believe it’s not one-sided growth, but a journey where both mainstream enterprises and crypto startups can bring on new opportunities.</p>
+       </div>
+      </div>
+      <div className="post">
+       <div className="image">
+       <img src="https://techcrunch.com/wp-content/uploads/2023/03/GettyImages-1180708314-e1679675797562.jpg?w=1390&crop=1" alt="image1"/>
+       </div>
+       <div className="text">
+       <h2>Web3 adoption could come via the enterprise, but the real boom will be through</h2>
+       <p className="info">
+        <a href="/" className="author" >  john cena </a>
+        <time>2023-01-6 14</time>
+       </p>
+         <p className="summary">As the race for enterprise adoption in web3 accelerates, some people believe it’s not one-sided growth, but a journey where both mainstream enterprises and crypto startups can bring on new opportunities.</p>
+       </div>
+      </div>
+    </main>
   );
 }
 

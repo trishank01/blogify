634625948a2e8b04debf2e1392ff7716977e49a5
tk <trishank01@gmail.com>
2023-09-18 23:59:05 +0530

    working on auth

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,23 +1,36 @@
 import React, { useState } from "react";
 
 const Login = () => {
- 
+  const [username, setUserName] = useState("");
+  const [password, setUserPassword] = useState("");
+  
+  
+  async function handleRegister(e) {
+    e.preventDefault();
+   await fetch("http://localhost:4000/register" , {
+      method : "POST",
+      body : JSON.stringify({username , password}),
+      headers : {'Content-Type' : "application/json"},
+    })
+   }
 
+   
   return (
-    <>
-      <form  className="login">
-        <h1>Login</h1>
-        <input
-          type="text"
-          placeholder="username"
-        
+    <> 
+      <form onSubmit={handleRegister} className="register">
+        <h1>Register</h1>
+        <input type="text" 
+        placeholder="username"  
+        value={username}
+        onChange={(e) => setUserName(e.target.value)}
         />
-        <input
-          type="password"
-          placeholder="password"
-          
+        <input 
+        type="password" 
+        placeholder="password"
+        value={password}
+        onChange={(e) => setUserPassword(e.target.value)}
         />
-        <button >Login</button>
+        <button>Login</button>
       </form>
     </>
   );

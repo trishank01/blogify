3c33da9d098e89525ef7eb2f6a5ef33ccc8c967d
tk <trishank01@gmail.com>
2023-09-18 23:46:59 +0530

    connecting to backend for auth

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,16 +1,26 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const Login = () => {
+ 
+
   return (
     <>
-      <form className="login">
-      <h1>Login</h1>
-        <input type='text' placeholder='username'/>
-        <input type='password' placeholder='password'/>
-        <button>Login</button>
+      <form  className="login">
+        <h1>Login</h1>
+        <input
+          type="text"
+          placeholder="username"
+        
+        />
+        <input
+          type="password"
+          placeholder="password"
+          
+        />
+        <button >Login</button>
       </form>
     </>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,12 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Register = () => {
+  const [username, setUserName] = useState("");
+  const [password, setUserPassword] = useState("");
+  
+  
+  async function handleRegister(e) {
+    e.preventDefault();
+   await fetch("http://localhost:4000/register" , {
+      method : "POST",
+      body : JSON.stringify({username , password}),
+      headers : {'Content-Type' : "application/json"},
+    })
+   }
+
+   
   return (
     <> 
-      <form className="register">
+      <form onSubmit={handleRegister} className="register">
         <h1>Register</h1>
-        <input type="text" placeholder="username" />
-        <input type="password" placeholder="password" />
+        <input type="text" 
+        placeholder="username"  
+        value={username}
+        onChange={(e) => setUserName(e.target.value)}
+        />
+        <input 
+        type="password" 
+        placeholder="password"
+        value={password}
+        onChange={(e) => setUserPassword(e.target.value)}
+        />
         <button>Register</button>
       </form>
     </>

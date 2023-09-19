26acff26629a12d90d84ac287d71c3daff7c534f
tk <trishank01@gmail.com>
2023-09-19 22:51:31 +0530

    Now user can register and login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,24 +1,36 @@
 import React, { useState } from "react";
+import { Navigate } from "react-router-dom";
 
 const Login = () => {
   const [username, setUserName] = useState("");
   const [password, setUserPassword] = useState("");
+  const [redirect , setRedirect] = useState(false)
+  console.log(redirect)
   
-  
-  async function handleRegister(e) {
+  async function handleLogin(e) {
     e.preventDefault();
-   await fetch("http://localhost:4000/register" , {
+  const response = await fetch("http://localhost:4000/login" , {
       method : "POST",
       body : JSON.stringify({username , password}),
       headers : {'Content-Type' : "application/json"},
+      credentials : "include",
     })
+    if(response.ok){
+      setRedirect(true)
+    }else {
+      alert("wrong credentials")
+     }
+     
    }
 
-   
+
+   if(redirect){
+    return <Navigate to="/"/>
+   }
   return (
     <> 
-      <form onSubmit={handleRegister} className="register">
-        <h1>Register</h1>
+      <form onSubmit={handleLogin} className="register">
+        <h1>Login</h1>
         <input type="text" 
         placeholder="username"  
         value={username}

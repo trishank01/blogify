1f6835045a7682e3cdc54f9591c4236902a00eb8
tk <trishank01@gmail.com>
2023-09-25 00:15:06 +0530

    created Blogpost model and api

--- index.js
+++ index.js
@@ -1,77 +1,110 @@
 const express = require("express");
 const cors = require("cors");
-const  mongoose = require("mongoose");
+const mongoose = require("mongoose");
 const User = require("./models/User");
-const bcrypt = require("bcrypt")
-const jwt = require("jsonwebtoken")
-const cookieParser = require("cookie-parser")
-const app = express()
+const Post = require("./models/Post");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
+const multer = require('multer');
+
+const fs = require("fs");
+
+const app = express();
+
+const salt = bcrypt.genSaltSync(10);
+const secret = "sdf934ndfkplps";
+
+app.use(
+  cors({
+    credentials: true,
+    origin: "http://localhost:3000",
+  })
+);
+app.use(express.json());
+app.use(cookieParser());
+
+const uploadMiddleware = multer({ dest: 'uploads/' });
+
+mongoose.connect(
+  "mongodb+srv://blog:KqlTO9WlGpW2UAo3@clusterblogpost.wurtrvt.mongodb.net/?retryWrites=true&w=majority"
+);
+
+app.post("/register", async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    const userDoc = await User.create({
+      username,
+      password: bcrypt.hashSync(password, salt),
+    });
+    res.json(userDoc);
+  } catch (error) {
+    res.status(400).json(error);
+  }
+});
+
+app.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+  const userDoc = await User.findOne({ username });
+  const passOk = bcrypt.compareSync(password, userDoc.password);
+  if (passOk) {
+    //logged in
+    jwt.sign({ username, id: userDoc._id }, secret, {}, (error, token) => {
+      if (error) throw error;
+      res.cookie("token", token).json({
+        id: userDoc._id,
+        username,
+      });
+    });
+  } else {
+    res.status(400).json("wrong credentials");
+  }
+});
+
+app.get("/profile", (req, res) => {
+  const { token } = req.cookies;
+  jwt.verify(token, secret, {}, (err, info) => {
+    if (err) throw err;
+    res.json(info);
+  });
+});
+
+app.post("/logout", (req, res) => {
+  res.cookie("token", "").json("ok");
+});
+
+app.post("/post", uploadMiddleware.single("file"), async (req, res) => {
+  const { originalname, path } = req.file;
+  const {token } = req.cookies
+  const parts = originalname.split(".");
+  const ext = parts[parts.length - 1];
+  const newPath = path + "." + ext;
+  fs.renameSync(path, newPath);
+
+  jwt.verify(token, secret, {}, async (err, info) => {
+    if (err) throw err;
+    const { title, summary, content } = req.body;
+    const PostDoc = await Post.create({
+      title,
+      summary,
+      content,
+      cover: newPath,
+      author : info.id
+    });
+    res.json(PostDoc);
+  });
+
+});
 
 
-const salt = bcrypt.genSaltSync(10)
-const secret = "sdf934ndfkplps"
-
-app.use(cors({
-    credentials : true, origin : "http://localhost:3000"
-}));
-app.use(express.json())
-app.use(cookieParser())
-
-mongoose.connect("mongodb+srv://blog:KqlTO9WlGpW2UAo3@clusterblogpost.wurtrvt.mongodb.net/?retryWrites=true&w=majority")
-
-app.post("/register" , async (req , res) => {
-    const {username , password} = req.body
-   
-    try {
-        const userDoc =  await User.create({ 
-            username,
-            password:bcrypt.hashSync(password , salt)
-        })
-        res.json(userDoc)
-    } catch (error) {
-        res.status(400).json(error)
-    }
+app.get('/post' , async(req , res) => {
+   const Posts = await Post.find()
+    res.json(Posts)
 })
-
-
-
-
-app.post("/login" , async (req , res) => {
-    const {username , password} = req.body
-    const userDoc = await User.findOne({username})
-    const passOk =  bcrypt.compareSync(password, userDoc.password)
-    if(passOk){
-      //logged in
-       jwt.sign({username , id: userDoc._id} , secret , {} , (error , token) => {
-         if(error) throw error;
-         res.cookie("token" , token).json({
-            id : userDoc._id,
-            username
-
-         })
-       })
-    }else{
-        res.status(400).json("wrong credentials")
-    }
-})
-
-
-app.get("/profile" , (req, res) => {
-    const {token} = req.cookies;
-    jwt.verify(token , secret , {}, (err , info) => {
-       if(err) throw err;
-       res.json(info)
-    })
-})
-
-
-app.post("/logout" , (req , res) => {
-    res.cookie("token" , '').json('ok')
-})
-
 //blog
 //KqlTO9WlGpW2UAo3
 
 //mongodb+srv://blog:KqlTO9WlGpW2UAo3@clusterblogpost.wurtrvt.mongodb.net/?retryWrites=true&w=majority
 
-app.listen(4000)
+app.listen(4000);
